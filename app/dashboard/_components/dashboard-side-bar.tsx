4b85da5c25f9a91a8341d0a052ea09d40efffa68
Nainish Rai <nainishraisjp@gmail.com>
2024-09-18 00:45:29 +0530

    first commit

---
+++ app/dashboard/_components/dashboard-side-bar.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import React, { useState } from "react"; // Import useState
+import { Separator } from "@/components/ui/separator";
+import clsx from "clsx";
+import {
+  Banknote,
+  Folder,
+  HomeIcon,
+  Settings,
+  ChevronRightCircle,
+  ChevronRight,
+  ChevronLeft,
+} from "lucide-react"; // Import ChevronRightCircle for toggle icon
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface NavItem {
+  label: string;
+  href: string;
+  icon: React.ComponentType<{ className?: string }>;
+}
+
+const navItems: NavItem[] = [
+  {
+    label: "Home",
+    href: "/dashboard",
+    icon: HomeIcon,
+  },
+  {
+    label: "Projects",
+    href: "/dashboard/projects",
+    icon: Folder,
+  },
+  {
+    label: "Finance",
+    href: "/dashboard/finance",
+    icon: Banknote,
+  },
+];
+const secondaryNavItems: NavItem[] = [
+  {
+    label: "Settings",
+    href: "/dashboard/settings",
+    icon: Settings,
+  },
+];
+
+export default function DashboardSideBar() {
+  const [collapsed, setCollapsed] = useState(false);
+  const pathname = usePathname();
+
+  const toggleSidebar = () => setCollapsed(!collapsed); // Toggle function
+
+  return (
+    <div
+      className={`transition-all duration-500 lg:block hidden border-r h-full ${
+        collapsed ? "w-16" : "w-64"
+      }`}
+    >
+      <div className="flex h-full max-h-screen flex-col">
+        <div className="flex h-[55px] items-center px-3 w-full justify-between border-b">
+          <button onClick={toggleSidebar} className="p-2">
+            <ChevronLeft
+              className={`transition-transform opacity-80 font- duration-200 ${
+                collapsed ? "rotate-180" : ""
+              }`}
+            />
+          </button>
+        </div>
+        <div className="flex-1 mt-2 overflow-auto">
+          <nav className="grid items-start px-2 text-sm font-medium">
+            {navItems.map((item) => (
+              <Link
+                key={item.href}
+                className={clsx(
+                  "flex items-center gap-2 rounded-lg p-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50",
+                  {
+                    "bg-gray-100 dark:bg-neutral-800 dark:text-gray-50":
+                      pathname === item.href,
+                  }
+                )}
+                href={item.href}
+              >
+                <div className="border rounded-lg dark:bg-black dark:border-gray-800 border-gray-400 p-1 bg-white">
+                  <item.icon className="h-4 w-4" />
+                </div>
+                {!collapsed && item.label}
+              </Link>
+            ))}
+            <Separator className="my-3" />
+            {secondaryNavItems.map((item) => (
+              <Link
+                key={item.href}
+                className={clsx(
+                  "flex items-center gap-2 rounded-lg p-2 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50",
+                  {
+                    "bg-gray-100 dark:bg-neutral-800 dark:text-gray-50":
+                      pathname === item.href,
+                  }
+                )}
+                href={item.href}
+              >
+                <div className="border rounded-lg dark:bg-black dark:border-gray-800 border-gray-400 p-1 bg-white">
+                  <item.icon className="h-4 w-4" />
+                </div>
+                {!collapsed && item.label}
+              </Link>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/homepage/side-by-side.tsx
@@ -0,0 +1,64 @@
+import { Leaf, User, SliceIcon } from "lucide-react";
+import { OrbitingCirclesComponent } from "./orbiting-circles";
+import { TITLE_TAILWIND_CLASS } from "@/utils/constants";
+
+const features = [
+  {
+    name: "Aesthetic Design",
+    description:
+      "Enjoy a visually pleasing workspace that promotes calmness and clarity.",
+    icon: Leaf,
+  },
+  {
+    name: "Focused Environment",
+    description:
+      "Eliminate distractions and harness your productivity in a serene digital space.",
+    icon: SliceIcon,
+  },
+  {
+    name: "Tailored for You",
+    description:
+      "Whether you're a student, professional, or creative, TheSpace adapts to fit your unique workflow.",
+    icon: User,
+  },
+];
+
+export default function SideBySide() {
+  return (
+    <div className="overflow-hidden ">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="lg:pr-8 lg:pt-4">
+            <div className="lg:max-w-lg">
+              <p
+                className={`${TITLE_TAILWIND_CLASS} mt-2 font-semibold tracking-tight dark:text-white text-gray-900`}
+              >
+                The Space: Your Digital Home
+              </p>
+              <p className="mt-6 leading-8 text-gray-600 dark:text-gray-400">
+                A new way to organize your life.
+              </p>
+              <dl className="mt-10 max-w-xl space-y-8 leading-7 text-gray-600 lg:max-w-none">
+                {features.map((feature) => (
+                  <div key={feature.name} className="relative pl-9">
+                    <dt className="inline font-semibold dark:text-gray-100 text-gray-900">
+                      <feature.icon
+                        className="absolute left-1 top-1 h-5 w-5"
+                        aria-hidden="true"
+                      />
+                      {feature.name}
+                    </dt>{" "}
+                    <dd className="inline dark:text-gray-400">
+                      {feature.description}
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+          <OrbitingCirclesComponent />
+        </div>
+      </div>
+    </div>
+  );
+}
